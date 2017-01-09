1cf1860dcb75adb26249b38124d5134fc9f4a03c
Jessé de Oliveira <jesse.oliveira.santos@gmail.com>
2017-01-09 02:52:24 -0200

    Atualizacao_Quality_Life
    Atualizacao_App

---
+++ app/src/main/java/com/example/jesse/aplicativoq/WriteActivity.java
@@ -0,0 +1,16 @@
+package com.example.jesse.aplicativoq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+    }
+}
+
+
+
